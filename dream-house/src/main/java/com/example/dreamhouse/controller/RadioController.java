3a6de4a61d0b967c51411ca0d69483bd029d3e56
HayashiDaiki-Man <millennialz2022@gmail.com>
2022-06-02 19:41:46 +0900

    update
    必要機能を編集

--- dream-house/src/main/java/com/example/dreamhouse/controller/RadioController.java
+++ dream-house/src/main/java/com/example/dreamhouse/controller/RadioController.java
@@ -5,6 +5,8 @@ import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -32,8 +34,13 @@ public class RadioController {
         model.addAttribute("houseSelection", houseSelection);
         model.addAttribute("gardenSelection", gardenSelection);
 
-        return "selection";
+        return "result";
 
+    }
+
+    @GetMapping("/result")
+    public String result(){
+        return "result";
     }
 
 }
